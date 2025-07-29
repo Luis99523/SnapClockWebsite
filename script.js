81dee6f66390de359540cb730c55a5bdd3f612f7
TheRedSparrow1 <vedansh2488@gmail.com>
2025-07-29 15:18:48 +0100

    TIme changing functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-script
+function changeTime(){
+            let currentDate = new Date();
+            let hours = currentDate.getHours().toString();
+            let minutes = currentDate.getMinutes().toString();
+            let seconds = currentDate.getSeconds().toString();
+
+            if (hours.length == 1){
+                "0".concat(hours);
+            };
+            if (minutes.length == 1){
+                "0".concat(minutes);
+            };
+            if (seconds.length == 1){
+                "0".concat(minutes);
+            };
+            
+            let currentTime = hours.concat(":", minutes, ":", seconds)
+
+            document.getElementById("currenttime").innerText = currentTime;
+        }
+
+setInterval(changeTime, 1000)
